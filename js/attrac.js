81903a7d5f8205053376873644d3db66727cc819
ZenBogdan2604 <93253276+ZenBogdan2604@users.noreply.github.com>
2024-10-25 14:42:55 +0300

    Add files via upload

---
+++ js/attrac.js
@@ -0,0 +1,73 @@
+// Pagination
+
+document.addEventListener('DOMContentLoaded', function () {
+    const content = document.querySelector('.content'); 
+    const itemsPerPage = 1; 
+    let currentPage = 0;
+    const items = Array.from(content.getElementsByTagName('section')).slice(0); 
+  
+  function showPage(page) {
+    const startIndex = page * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    items.forEach((item, index) => {
+      item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+    });
+    updateActiveButtonStates();
+  }
+  
+  function createPageButtons() {
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    const paginationContainer = document.createElement('div');
+    const paginationDiv = document.body.appendChild(paginationContainer);
+    paginationContainer.classList.add('pagination');
+  
+    for (let i = 0; i < totalPages; i++) {
+      const pageButton = document.createElement('button');
+      pageButton.textContent = i + 1;
+      pageButton.addEventListener('click', () => {
+        currentPage = i;
+        showPage(currentPage);
+        updateActiveButtonStates();
+      });
+  
+        content.appendChild(paginationContainer);
+        paginationDiv.appendChild(pageButton);
+      }
+  }
+  
+  function updateActiveButtonStates() {
+    const pageButtons = document.querySelectorAll('.pagination button');
+    pageButtons.forEach((button, index) => {
+      if (index === currentPage) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  }
+  
+    createPageButtons();
+    showPage(currentPage);
+  });
+
+
+
+
+  let menuBtn = document.querySelector('.header__menu-btn-burger');
+  let menu = document.querySelector('.nav');
+  let menuItem = document.querySelectorAll('.nav__link');
+  
+  menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  })
+  
+  
+  menuItem.forEach (function(menuItem) {
+    menuItem.addEventListener('click',function(){
+            menuBtn.classList.toggle('active');
+            menu.classList.toggle('active');
+    })
+  })
+  
+  
